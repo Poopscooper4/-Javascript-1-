74f22744faaa13f3c15b654c5a197b6572d769a2
Rati Elizbarashvili <elizbarashvilirati15@Gmail.com>
2024-10-18 03:05:39 +0400

    DONE ✅

---
+++ script.js
@@ -0,0 +1,35 @@
+// const firstname = "rati"
+
+// const Gamil = "elizbarashvilirati15@Gmai.com"
+
+// const Phone = +995598250185
+
+
+// let age = 19;
+
+// let agechecked = true
+
+// console.log(age)
+
+const Person = {
+
+    firstname : "Rati",
+    lastname : "Elizbarashvili" ,
+    Gmail : "elizbarashvilirati15@Gmai.com" , 
+    Phone : "+995598250185"
+}
+let Personage = {
+    age : 19,
+    agechecked : true,
+}
+
+
+console.log(Person)
+
+
+console.log(typeof Person)
+
+console.log(Personage)
+
+
+
